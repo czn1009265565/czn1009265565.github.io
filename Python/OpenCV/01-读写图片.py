bb2bd6e34af6f9c84c22a6e8d64c86a3796635f1
zenan <1009265565@qq.com>
2024-12-30 10:48:36 +0800

    [ADD] 新增剪裁图片

--- Python/OpenCV/01-读写图片.py
+++ Python/OpenCV/01-读写图片.py
@@ -2,14 +2,15 @@ import cv2
 
 """
 读取、展示、保存图片
-- imread(filename, flags=None)        读取图片
+- imread(filename, flags=None)        读取图片，返回一个BGR形式的图像对象，其类型为一个numpy数组。flags枚举见ImreadModes
 - imshow(winname, mat)                展示图片
 - imwrite(filename, img, params=None) 保存图片
 
-ImreadModes图片读取模式分为以下三种
-- cv2.IMREAD_COLOR      默认参数，以彩色模式读取图像，忽略 alpha 通道。对应的数值是 1
-- cv2.IMREAD_GRAYSCALE  以灰度模式读取图像，将图像转换为单通道灰度图像。对应的数值是 0
-- cv2.IMREAD_UNCHANGED  读取包括 alpha 通道（透明度）在内的所有图像信息。对于带有 alpha 通道的图像（如 PNG 图像），将保持完整信息。对应的数值是 -1
+
+ImreadModes 图片读取模式分为以下三种
+  - cv2.IMREAD_COLOR      默认参数，以彩色模式读取图像，忽略 alpha 通道。对应的数值是 1
+  - cv2.IMREAD_GRAYSCALE  以灰度模式读取图像，将图像转换为单通道灰度图像。对应的数值是 0
+  - cv2.IMREAD_UNCHANGED  读取包括 alpha 通道（透明度）在内的所有图像信息。对于带有 alpha 通道的图像（如 PNG 图像），将保持完整信息。对应的数值是 -1
 """
 
 if __name__ == "__main__":
---
+++ Python/OpenCV/04-剪裁图片.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+"""
+剪裁图片
+
+img[start_row:end_row, start_col:end_col]
+"""
+
+if __name__ == "__main__":
+    img = cv2.imread('Resources/book.jpg')
+    print(img.shape)
+    cv2.imshow("original", img)
+
+    # 剪裁图片
+    cropped_image = img[80:280, 150:330]
+
+    # 展示剪裁后的图片
+    cv2.imshow("cropped", cropped_image)
+
+    # 保存剪裁后的图片
+    cv2.imwrite("Cropped Image.jpg", cropped_image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
